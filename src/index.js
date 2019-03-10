6e14d30f4476a6e451ffb9890918bfd9e3829484
chitan <chitans@gmail.com>
2019-03-10 23:09:24 +0900

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Roulette extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 1,
+            isStart: false};
+        this.toggleButton = this.toggleButton.bind(this);
+    }
+
+    shuffle(){
+        var rand = Math.floor(Math.random()*30) + 1;
+        this.setState({number: rand});
+    }
+
+    startRoulette(){
+        this.interval = setInterval(() => this.shuffle(), 50);
+    }
+
+    stopRoulette(){
+        clearInterval(this.interval);
+    }
+
+    toggleButton(){
+        if(this.state.isStart){
+            this.stopRoulette();
+        }else{
+            this.startRoulette();
+        }
+        this.setState((prevState) => ({
+            isStart : !prevState.isStart
+        }));
+    }
+
+    render(){
+        return(
+            <div>
+                <p>number: {this.state.number}</p>
+                <button onClick={this.toggleButton}>
+                    {this.state.isStart ? 'Stop' : 'Start'}
+                </button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Roulette />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
